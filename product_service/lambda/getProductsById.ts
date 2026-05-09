e0f81763628be7b4995514bbab6f0563de9c9f4d
Oleksii Roman <fprognoz@gmail.com>
2026-05-09 21:48:27 +0300

    feat: update lambdas to get info from DynamoDB

--- product_service/lambda/getProductsById.ts
+++ product_service/lambda/getProductsById.ts
@@ -1,12 +1,37 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { products } from "../mock/products.js";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const productId = event.pathParameters?.productId;
 
-  const product = products.find((p) => p.id === productId);
+  if (!productId) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ message: "Product ID is required" }),
+    };
+  }
 
-  if (!product) {
+  const productsTable = process.env.PRODUCTS_TABLE!;
+  const stocksTable = process.env.STOCKS_TABLE!;
+
+  const [productResult, stockResult] = await Promise.all([
+    docClient.send(new GetCommand({ TableName: productsTable, Key: { id: productId } })),
+    docClient.send(new QueryCommand({
+      TableName: stocksTable,
+      KeyConditionExpression: "product_id = :pid",
+      ExpressionAttributeValues: { ":pid": productId },
+    })),
+  ]);
+
+  if (!productResult.Item) {
     return {
       statusCode: 404,
       headers: {
@@ -16,6 +41,9 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       body: JSON.stringify({ message: "Product not found" }),
     };
   }
+
+  const stock = stockResult.Items?.[0];
+  const product = { ...productResult.Item, count: stock?.count ?? 0 };
 
   return {
     statusCode: 200,
--- product_service/lambda/getProductsList.ts
+++ product_service/lambda/getProductsList.ts
@@ -1,7 +1,25 @@
 import { APIGatewayProxyResult } from "aws-lambda";
-import { products } from "../mock/products.js";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
+  const productsTable = process.env.PRODUCTS_TABLE!;
+  const stocksTable = process.env.STOCKS_TABLE!;
+
+  const [productsResult, stocksResult] = await Promise.all([
+    docClient.send(new ScanCommand({ TableName: productsTable })),
+    docClient.send(new ScanCommand({ TableName: stocksTable })),
+  ]);
+
+  const stocks = stocksResult.Items ?? [];
+  const products = (productsResult.Items ?? []).map((product) => {
+    const stock = stocks.find((s) => s.product_id === product.id);
+    return { ...product, count: stock?.count ?? 0 };
+  });
+
   return {
     statusCode: 200,
     headers: {
